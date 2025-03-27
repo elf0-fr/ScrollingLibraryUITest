e766d86e92037ac5da6a1a5f09b9f4734011a643
Romain Tirbisch <romain_tirbisch@yahoo.com>
2025-03-27 13:19:12 +0100

    Initial Commit

---
+++ ScrollingLibraryUITest/ScrollingLibraryUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingLibraryUITestApp.swift
+//  ScrollingLibraryUITest
+//
+//  Created by Elfo on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingLibraryUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
